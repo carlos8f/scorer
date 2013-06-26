52cb27e59194100726022431a52f6e35bcdbfb56
Carlos Rodriguez <carlos@s8f.org>
2013-06-26 12:50:42 -0700

    add test for short word

--- test/basic.js
+++ test/basic.js
@@ -7,4 +7,8 @@ describe('basic test', function () {
     var score = scorer(10);
     assert.strictEqual(score('Carlos Rodriguez').toNumber(), 67097114108111115032082111100);
   });
+  it('short word', function () {
+    var score = scorer(6);
+    assert.strictEqual(score('blah').toNumber(), 98108097104000000);
+  });
 });
